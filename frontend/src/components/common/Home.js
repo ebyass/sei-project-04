f6681da4c67f36c842c5fb5957f14b161bab63ab
YardenLawson <yardenlawson@gmail.com>
2020-06-08 15:45:04 +0100

    home

--- frontend/src/components/common/Home.js
+++ frontend/src/components/common/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { getAllPosts } from '../../lib/api'
 import useFetch from '../../utils/useFetch'
 import { Redirect } from 'react-router-dom'
@@ -6,26 +6,47 @@ import Spinner from '../common/Spinner'
 
 
 function Home() {
-  // const history = useHistory()
 
   //* data in this context is what is being returned from useFetch. What we are doing here is renaming data: with post. So we don't need to use this.state. Everything is on post.
-  const { data: post, loading, error } = useFetch(getAllPosts)
+  const { data: post, error } = useFetch(getAllPosts)
+  const [ recentPost, setRecentPost ] = useState('') //* setting state here.  
+
+  useEffect(() => {
+    const max = post ? post.length - 1 : null //* making the result ternary. Doing it here rather than the render, using the varaible to specify. Only creates variable max with post length if post exists. (-1 becaue length is not the same as the index number)
+    console.log('this is the max', max)
+    console.log('this is the post in the function', post)
+    // console.log('post[2]', post[2])
+    const recentPost = max ? post[max] : null
+    console.log(recentPost)
+    setRecentPost(recentPost) //* sets post with index that has the greatest value to state -> it can be resued using recentPost
+  
+  },[post]) //* every time post changes. It will trigger this function to run
 
   if (error) {
     return <Redirect to="/notfound" />
   }
 
-  // if (!this.state) return null
-
-  console.log('home state', post)
-
-
   return (
     <div>
-      {loading ?
-        <Spinner />
+      <h1>FAM</h1>
+      <p>
+        About 
+      best app init
+      </p>
+      {recentPost ? //* use recentPost here instead of loading
+      //* anything you put before colon will be loaded. If there's nothing there it render the loading spinner.
+        <div>
+          <h1>{recentPost.title}</h1>
+          <h2>{recentPost.info}</h2>
+          <h3>{recentPost.film_title}</h3>
+          <img src={recentPost.image_film} />
+          <h3>{recentPost.art_title}</h3>
+          <img src={recentPost.image_art} />
+          <h3>{recentPost.music_title}</h3>
+          <img src={recentPost.image_music} />
+        </div>
         :
-        <h1>{post[0].id}</h1>
+        <Spinner />
       }
     </div>
   )
@@ -35,32 +56,3 @@ function Home() {
 
 export default Home
 
-
-
-
-
-
-
-
-
-//   const { data: post, loading, error } = useFetch(getSinglePost)
-
-
-//   if (error) {
-//     console.log(error.data.message)
-//     console.log('this is the posts', post)
-    
-//     return <Redirect to="/notfound" />
-//   }
-
-//   return (
-//     <div>
-//       {loading ?
-//         <Spinner />
-//         :
-//         post.map(post => (
-//           <p key={post.id} {...post.id} />
-//         ))
-//       }
-//     </div>
-//   )
--- posts/models.py
+++ posts/models.py
@@ -5,7 +5,12 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     date_posted = models.DateTimeField()
     info = models.CharField(max_length=1000)
-    image = models.CharField(max_length=400, blank=True, null=True)
+    image_film = models.CharField(max_length=400)
+    image_art = models.CharField(max_length=400)
+    image_music = models.CharField(max_length=400)
+    film_title = models.CharField(max_length=50)
+    art_title = models.CharField(max_length=50)
+    music_title = models.CharField(max_length=50)
 
 #! MANY TO MANY
     mediums = models.ManyToManyField(
