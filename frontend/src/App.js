8a5d1a9d67a63aa7458c7de937e77c72b8ae33ad
YardenLawson <yardenlawson@gmail.com>
2020-06-14 12:40:01 +0100

    favicon update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Home from './components/common/Home'
 import Navbar from './components/common/Navbar'
 import Footer from './components/common/Footer'
 import ErrorPage from './components/common/Error'
-// import SecureRoute from './components/common/SecureRoute' //! add this in ?
+import SecureRoute from './components/common/SecureRoute' 
 import FilmIndex from './components/mediums/FilmIndex'
 import ArtIndex from './components/mediums/ArtIndex'
 import MusicIndex from './components/mediums/MusicIndex'
@@ -17,8 +17,6 @@ import Favourites from './components/common/Favourites'
 console.log('%cThis app was built by Eleanor & Yarden, check us out on github:', 'color:#C8A2C8;font-size:20px;font-weight:bold')
 console.log('%cEleanor: github.com/ebyass \nYarden: github.com/YBL123', 'color:#8BD6BB;font-size:16px')
 
-
-//* add secureRoute to profile page when it is added 
 
 function App() {
   return (
@@ -34,7 +32,7 @@ function App() {
             <Route path="/music" component={MusicIndex} />
             <Route path="/register" component={Register} />
             <Route path="/login" component={Login} />
-            <Route path="/profile" component={Favourites} />
+            <SecureRoute path="/profile" component={Favourites} />
             <Route path="/*" component={ErrorPage} />
           </Switch>
           <Footer />
